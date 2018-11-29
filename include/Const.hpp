111b008e6d1a210514a820b509d07a160de267c1
Alex <alex.bergmann87@gmail.com>
2018-11-29 23:47:42 +0100

    const file add

---
+++ include/Const.hpp
@@ -0,0 +1,17 @@
+/*
+   CNosh - Firmware for ESP32 based cat food dispenser with Wi-Fi control
+   Written by 
+   - Alexander Bergmann (alexander.bergmann@fh-bielefeld.de)
+   - Dario Leunig (dleunig@fh-bielefeld.de)
+   
+   Licensed under GPLv3. See LICENSE for details.
+   */
+
+#ifndef CONST_h
+#define CONST_h
+
+// LCD
+static int lcdColumns = 16;
+static int lcdRows = 2;
+
+#endif
