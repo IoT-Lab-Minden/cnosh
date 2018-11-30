c2d922d18b4750f8f892ce33ea22cc0ef379229d
Dario Leunig <daleunig@gmail.com>
2018-11-30 23:47:50 +0100

    RFID cpp add

---
+++ src/RFID.cpp
@@ -0,0 +1,30 @@
+/*
+   RFID - Firmware for ESP32 based cat food dispenser with Wi-Fi control
+   Written by 
+   - Alexander Bergmann (alexander.bergmann@fh-bielefeld.de)
+   - Dario Leunig (dleunig@fh-bielefeld.de)
+   
+   Licensed under GPLv3. See LICENSE for details.
+   */
+
+#include <RFID.hpp>
+
+RFID::RFID() {
+    rfid = new MFRC522(SS_PIN, RST_PIN);
+}
+
+bool RFID::init() {
+
+}
+
+void RFID::dumpToSerial() {
+
+}
+
+int RFID::detectUnit() {
+
+}
+
+bool RFID::readUnitSerial() {
+
+}
