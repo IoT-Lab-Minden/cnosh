a12eea6539cda0f66db27c0de7dfb19313714a8b
Dario Leunig <daleunig@gmail.com>
2018-12-02 12:50:04 +0100

    Time cpp add

---
+++ src/Time.cpp
@@ -0,0 +1,27 @@
+/*
+   CNosh - Firmware for ESP32 based cat food dispenser with Wi-Fi control
+   Written by 
+   - Alexander Bergmann (alexander.bergmann@fh-bielefeld.de)
+   - Dario Leunig (dleunig@fh-bielefeld.de)
+   
+   Licensed under GPLv3. See LICENSE for details.
+   */
+
+#include <Time.hpp>
+
+Time::Time() {
+    WiFiUDP ntpUDP;
+    NTPClient timeClient(ntpUDP);
+}
+
+bool Time::init() {
+
+}
+
+String Time::getDate() {
+    
+}
+
+String Time::getTime() {
+    
+}
