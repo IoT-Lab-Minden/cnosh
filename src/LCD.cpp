b3d805dd3124a53102357b1c46c49902615cae0f
Alex <alex.bergmann87@gmail.com>
2018-12-04 12:30:35 +0100

    lcd methods test upd

--- src/LCD.cpp
+++ src/LCD.cpp
@@ -10,17 +10,25 @@
 #include <LCD.hpp>
 
 LCD::LCD() {
-    lcd = new LiquidCrystal_I2C(0x3F, lcdColumns, lcdRows); 
+	lcd = new LiquidCrystal_I2C(0x3F, lcdColumns, lcdRows); 
 }
 
 bool LCD::init() {
+	// initialize LCD
+	lcd.init();
+	// turn on LCD backlight
+	lcd.backlight();
 
+	Serial.begin(115200);
+	return true;
 }
 
 void LCD::printLine(String text, int row) {
-    
+	lcd.setCursor(0, row);
+	// print message
+	lcd.print(text);
 }
 
 void LCD::clear() {
-
+	lcd.clear();
 }
