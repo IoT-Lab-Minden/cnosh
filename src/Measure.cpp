82eca18a61cb642da7b460853f3dc1591e6dacd6
Alex <alex.bergmann87@gmail.com>
2018-12-04 12:30:22 +0100

    measure methods test upd

--- src/Measure.cpp
+++ src/Measure.cpp
@@ -15,9 +15,34 @@ Measure::Measure() {
 }
 
 bool Measure::init() {
+	// wait until serial port opens for native USB devices
+	while (!Serial)
+	{
+		delay(1);
+	}
+
+	Serial.println("Adafruit VL53L0X test");
+	if (!lox.begin())
+	{
+		Serial.println(F("Failed to boot VL53L0X"));
+		while (1);
+	}
 
 }
 
 double Measure::readDistance() {
+	Serial.print("Reading a measurement... ");
+	lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
+	if (measure.RangeStatus != 4)
+	{ // phase failures have incorrect data
+		Serial.print("Distance (mm): ");
+		Serial.println(measure.RangeMilliMeter);
+	}
+	else
+	{
+		Serial.println(" out of range ");
+	}
+
+	delay(100);
 }
